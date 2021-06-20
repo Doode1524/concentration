915ef64f1820a47943621fea582a0fc9b763751a
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-20 11:29:50 -0500

    created file

---
+++ src/class_components/banner.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react' 
+import Concentration from '../functional_components/concentration'
+
+class Banner extends Component {
+
+  render() {
+    return(
+      <div>
+        <Concentration />
+      </div>
+      
+    )
+  }
+
+
+}
+
+export default Banner;
