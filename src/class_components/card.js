ec44e2a358695f6dd1d20ba3547111751c5b03a6
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-20 11:41:36 -0500

    created file

---
+++ src/class_components/card.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react' 
+
+
+class Card extends Component {
+
+  render() {
+    return(
+      <div>
+        card
+      </div>
+    )
+  }
+}
+
+export default Card;
