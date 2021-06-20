8a943905063c2f769b6ac0248315a58b84999878
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-20 11:29:17 -0500

    created file

---
+++ src/functional_components/concentration.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+const Concentration = () => {
+  return(
+    <div>
+      <p><h1>Concentration</h1></p>
+      <hr></hr>
+    </div>
+    
+    
+  )
+}
+
+export default Concentration;
