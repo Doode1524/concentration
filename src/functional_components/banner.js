28e528bf83a6b945c03b509bae75be7350a6d635
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-30 19:04:19 -0500

    added a container, removed some lines

--- src/functional_components/banner.js
+++ src/functional_components/banner.js
@@ -1,30 +1,25 @@
 import React from 'react' 
 import NavBar from './nav';
 import Home from './home';
-import GamesList from '../class_components/gamesList';
+import GamesContainer from '../class_components/gamesContainer';
 import NewGame from '../class_components/newGame';
+
 import {
-  BrowserRouter as Router,
-  Route
+  BrowserRouter as Router, Route, Switch
 } from 'react-router-dom';
 
 const Banner = () => {
 
   return(
     <div className='banner'>
-
       <Router>
 
         <NavBar />
-
         <Route exact path="/" component={Home} />
-
-        <Route path="/newGame" component={NewGame} />
-
-        <Route path="/games" render={() => <GamesList />} />
-
-        {/* <Route exact path="/games/:id" render={routerProps => <GameView games={games} {...routerProps} />} /> */}
-
+        <Route exact path="/games" component={(routeInfo) => <GamesContainer routeData={routeInfo} />} />
+        <Route exact path="/games/new">
+          <NewGame />
+          </Route> 
       </Router>
       
       <hr style={{color: "black"}}></hr>
