89f1a0a74cc722a155703bdf42db4944367ebc41
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-30 19:02:02 -0500

    moved this reducer

--- src/redux/reducers/gamesReducer.js
+++ src/redux/reducers/gamesReducer.js
@@ -1,12 +1,12 @@
 export default function games(state=[], action) {
   switch (action.type){
-    case 'FETCH_GAMES':
+    case 'SET_GAMES':
       //return a list of all games
-      return action.payload.games;
+      return action.payload;
   
     case 'NEW_GAME':
       //return a game id
-      return action.payload.game;
+      return [...state, action.payload];
   
     default:
       return state;  
