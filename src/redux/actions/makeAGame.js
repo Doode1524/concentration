c078c492b73d7065a7a2f9a7e12a21a34f785ab6
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-06-30 19:03:26 -0500

    fixed this so it works

--- src/redux/actions/makeAGame.js
+++ src/redux/actions/makeAGame.js
@@ -1,13 +1,13 @@
-const MakeAGame = (game) => {
+const makeAGame = (gameData) => {
   return (dispatch) => {
   fetch('http://127.0.0.1:3001/games',{
     method: 'post',
-    body: JSON.stringify(game)
+    body: JSON.stringify(gameData)
   }).then(r => r.json())
     .then(game => {
-      dispatch({type: "NEW_GAME", payload: {game}})
+      dispatch({type: "NEW_GAME", payload: game})
   })
 }
 }
 
-export { MakeAGame }
+export { makeAGame }
