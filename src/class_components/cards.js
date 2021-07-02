ad22f49844fc42e363a64518161e6ad8cd529051
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-07-02 15:32:34 -0500

    changed the imgs to state

--- src/class_components/cards.js
+++ src/class_components/cards.js
@@ -15,47 +15,70 @@ import img10 from '../assets/Rooster.png'
 import img11 from '../assets/Moose.webp'
 import img12 from '../assets/Wolf.jpg'
 
-const imgFts = [
-  {index: '01', name:'Boar', image: img01},
-  {index: '02', name:'Boar', image: img01},
-  {index: '03', name:'Cat', image: img02},
-  {index: '04', name:'Cat', image: img02},
-  {index: '05', name:'Cow', image: img03},
-  {index: '06', name:'Cow', image: img03},
-  {index: '07', name:'Deer', image: img04},
-  {index: '08', name:'Deer', image: img04},
-  {index: '09', name:'Ducks', image: img05},
-  {index: '10', name:'Ducks', image: img05},
-  {index: '11', name:'Goose', image: img06},
-  {index: '12', name:'Goose', image: img06},
-  {index: '13', name:'Horse', image: img07},
-  {index: '14', name:'Horse', image: img07},
-  {index: '15', name:'Moose', image: img11},
-  {index: '16', name:'Moose', image: img11},
-  {index: '17', name:'Piglet', image: img08},
-  {index: '18', name:'Piglet', image: img08},
-  {index: '19', name:'Puppy', image: img09},
-  {index: '20', name:'Puppy', image: img09},
-  {index: '21', name:'Rooster', image: img10},
-  {index: '22', name:'Rooster', image: img10},
-  {index: '23', name:'Wolf', image: img12},
-  {index: '24', name:'Wolf', image: img12}
-]
 
 class Cards extends Component {
+  
+  state = {
+    click: 0,
+    imgFts: [
+      {index: '01', name:'Boar', image: img01, flipped: false},
+      {index: '02', name:'Boar', image: img01, flipped: false},
+      {index: '03', name:'Cat', image: img02, flipped: false},
+      {index: '04', name:'Cat', image: img02, flipped: false},
+      {index: '05', name:'Cow', image: img03, flipped: false},
+      {index: '06', name:'Cow', image: img03, flipped: false},
+      {index: '07', name:'Deer', image: img04, flipped: false},
+      {index: '08', name:'Deer', image: img04, flipped: false},
+      {index: '09', name:'Ducks', image: img05, flipped: false},
+      {index: '10', name:'Ducks', image: img05, flipped: false},
+      {index: '11', name:'Goose', image: img06, flipped: false},
+      {index: '12', name:'Goose', image: img06, flipped: false},
+      {index: '13', name:'Horse', image: img07, flipped: false},
+      {index: '14', name:'Horse', image: img07, flipped: false},
+      {index: '15', name:'Moose', image: img11, flipped: false},
+      {index: '16', name:'Moose', image: img11, flipped: false},
+      {index: '17', name:'Piglet', image: img08, flipped: false},
+      {index: '18', name:'Piglet', image: img08, flipped: false},
+      {index: '19', name:'Puppy', image: img09, flipped: false},
+      {index: '20', name:'Puppy', image: img09, flipped: false},
+      {index: '21', name:'Rooster', image: img10, flipped: false},
+      {index: '22', name:'Rooster', image: img10, flipped: false},
+      {index: '23', name:'Wolf', image: img12, flipped: false},
+      {index: '24', name:'Wolf', image: img12, flipped: false}
+    ]
+  }
+
+  // someMethod = () => {
+  //   this.setState
+  // }
+
+
+  // shuffle() {
+
+  //   for (let i = array.length - 1; i > 0; i--) {
+  //       let j = Math.floor(Math.random() * (i + 1));
+            
+  //       let temp = array[i];
+  //       array[i] = array[j];
+  //       array[j] = temp;
+  //   }
+  // }
 
   render() {
     return(
       <div style={{marginTop: '30px'}}>
 
+      
+
       <Grid container>
          <Grid item container direct='row' justify='space-evenly'>  
-          {imgFts.map((img) => (
+          {this.state.imgFts.map((img) => (
             <PlayingCard 
               key={img.index} 
               front={img.image} 
               name={img.name} 
               index={img.index}
+              flipped={img.flipped}
             />
           ))}
         </Grid>
