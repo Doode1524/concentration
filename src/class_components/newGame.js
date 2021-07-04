e787e486f4fcbdcb62180dd054b531d29af1a13b
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-07-04 12:54:30 -0500

    added a conditional, still in progress

--- src/class_components/newGame.js
+++ src/class_components/newGame.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react' 
 import { connect } from 'react-redux';
 import { makeAGame } from '../redux/actions/makeAGame';
+import Cards from './cards';
 
 class NewGame extends Component {
   state = {
     initials: "",
-    numPics: "8"
+    numPics: "selectValue"
   }
 
    handleChange = (e) => {
@@ -20,48 +21,54 @@ class NewGame extends Component {
   }
 
   render (){
-    return(
-      <div>
-        <h2>Play a New Game</h2>
-        <form className="newGame" onSubmit={this.handleSubmit}>
-          <label>Initials  </label>
-          <input 
-            type='text'
-            name='initials'
-            placeholder='TTT'
-            value={this.state.initials} 
-            onChange={this.handleChange}
-          ></input>
-          <br></br>
-          <br></br>
-          <label>Number of Pictures (*2 for total cards)   </label>
-          
-          <select 
-            name='numPics' 
-            value={this.state.numPics} 
-            onChange={this.handleChange}
-          >       
-            <option value="8">4</option>
-            <option value="10">5</option>
-            <option value="12">6</option>
-            <option value="14">7</option>
-            <option value="16">8</option>
-            <option value="18">9</option>
-            <option value="20">10</option>
-            <option value="22">11</option>
-            <option value="24">12</option>
-          </select>
-          <br></br>
-          <br></br>
-          <input 
-            style={{backgroundColor: 'darkblue', color: 'white'}}
-            type="submit"
-            value='Make a New Game'
-          ></input>
-          <br></br>
-        </form>
-      </div>
-    )
+    {if ( this.state.initials === "" || this.state.numPics === "selectValue"){
+    return (
+        <div>
+          <h2>Play a New Game</h2>
+            <form className="newGame" onSubmit={this.handleSubmit}>
+              <label>Initials  </label>
+              <input 
+                type='text'
+                name='initials'
+                placeholder='TTT'
+                value={this.state.initials} 
+                onChange={this.handleChange}
+              ></input>
+              <br></br>
+              <br></br>
+              <label>Number of Matches (* 2 for total cards)   </label>
+              <select 
+                name='numPics' 
+                value={this.state.numPics} 
+                onChange={this.handleChange}
+              > 
+                <option value="selectValue">Select An Option</option>
+                <option value="6">3</option>      
+                <option value="8">4</option>
+                <option value="10">5</option>
+                <option value="12">6</option>
+                <option value="14">7</option>
+                <option value="16">8</option>
+                <option value="18">9</option>
+              </select>
+              <br></br>
+              <br></br>
+              <input 
+                style={{backgroundColor: 'darkblue', color: 'white'}}
+                type="submit"
+                value='Make a New Game'
+              ></input>
+              <br></br>
+            </form>
+          </div>
+        )
+      } else {
+        <Cards />
+      }
+    }
+
+
+        
   }
 }
 export default connect((state) => {
