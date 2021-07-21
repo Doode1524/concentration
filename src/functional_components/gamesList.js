19b6e58777ab2344474d0f9ef671aeb730bc6d20
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-07-21 13:18:02 -0500

    2nd assessment live code

--- src/functional_components/gamesList.js
+++ src/functional_components/gamesList.js
@@ -5,12 +5,42 @@ import GamesListH2 from './gamesListH2'
 
 class GamesList extends Component {
 
+  state = {
+    games: []
+  }
+
+  clickHandler = () => {
+    function compare(a,b) {
+        const scoreA = a.score;
+        const scoreB = b.score;
+
+        let comparison = 0;
+      if (scoreA > scoreB) {
+        comparison = 1;
+      } else if (scoreA < scoreB) {
+        comparison = -1;
+      }
+      return comparison *-1;
+    }
+
+    this.setState({
+      games: this.props.games.sort(compare)
+    })
+
+
+  }
 
   render () {
     return (
       <>
         <GamesListH2 />
-        {this.props.games.map((g) => {
+        <button onClick={this.clickHandler}>Sort by Score</button>
+
+        {this.state.games > 0 ?
+        this.state.games.map((g) => {
+          return <p key={g.id}>{g.initials} {g.score} of {`${g.numCards}`/2} points <LikeButton /></p> 
+        }) 
+        : this.props.games.map((g) => {
           return <p key={g.id}>{g.initials} {g.score} of {`${g.numCards}`/2} points <LikeButton /></p> 
         })}
       </>
